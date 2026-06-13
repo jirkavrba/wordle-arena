341b100381b9e18cc0ddb27491bd47fdbd390836
JVancata <jakub.vancata@pm.me>
2026-06-13 22:37:03 +0200

    feature: add horsle bot

--- src/bots/bots.ts
+++ src/bots/bots.ts
@@ -2,11 +2,13 @@ import type { ASolverBot } from '../bot/ASolverBot.ts';
 import type { Wordle } from '../nytimes/Wordle.ts';
 
 import { RandomSolverBot } from './randomBot/RandomSolverBot.ts';
+import { HorsleBot } from './horsleBot/HorsleBot.ts';
 
 export const createAllBots = (wordle: Wordle): ASolverBot[] => {
   // add your bot here:
   return [
     new RandomSolverBot(wordle),
+    new HorsleBot(wordle),
     // new YourBot(wordle),
   ];
 };
---
+++ src/bots/horsleBot/HorsleBot.ts
@@ -0,0 +1,19 @@
+import { ASolverBot, type BotMeta } from '../allowedContext.ts';
+
+export class HorsleBot extends ASolverBot {
+  protected _about(): BotMeta {
+    return {
+      name: 'Horsle',
+      author: 'Knedla',
+      description: 'This horse is super hard',
+    };
+  }
+
+  protected _init(): void {
+    console.log("Ready to horse around!")
+  }
+
+  protected _pickWord(): string {
+    return "horse";
+  }
+}
