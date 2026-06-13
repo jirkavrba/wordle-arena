a20500dc6662ce1e645f5ef601543e6423505a7d
JVancata <jakub.vancata@pm.me>
2026-06-13 22:38:22 +0200

    fix: description typo

--- src/bots/horsleBot/HorsleBot.ts
+++ src/bots/horsleBot/HorsleBot.ts
@@ -5,7 +5,7 @@ export class HorsleBot extends ASolverBot {
     return {
       name: 'Horsle',
       author: 'Knedla',
-      description: 'This horse is super hard',
+      description: 'This horse game is super hard',
     };
   }
 
