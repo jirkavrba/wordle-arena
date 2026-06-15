0e21223dcb0235266b1b4c617efe6996385d65a3
Jiří Vrba <jiri@vrba.dev>
2026-06-15 12:45:04 +0200

    Cheating bot 2.0

--- src/bots/cheatingBot/CheatingBot.ts
+++ src/bots/cheatingBot/CheatingBot.ts
@@ -1,26 +1,12 @@
 import { ASolverBot, type BotMeta } from '../allowedContext.ts';
 
-/**
- * This is a template for making your own solver bot. Copy this folder and change its name.
- *
- * Rules:
- * - Don't override any base methods (that includes the constructor).
- * - Implement all three abstract methods (_about, _init and _pickWord)
- * - Only import from '../allowedContext.ts', this folder and custom libraries.
- * - After creating your bot, add it into '../bots.ts'.
- * - Use only the provided utility methods from ASolverBot
- * - Feel free to add your files :)
- * - (optional) Document your bot for others :)
- *
- * Provided utility methods:
- * - _isWordValid -> checks if the provided word is in the word list
- * - _getValidWords -> returns the entire word list
- *
- * Using utility methods gets recorded for fun :).
- */
-export class CheatingBot extends ASolverBot {
-  protected correctWord?: string;
+type FakeItTillYouMakeIt = {
+  _wordle: {
+    _solution: string[];
+  };
+};
 
+export class CheatingBot extends ASolverBot {
   protected _about(): BotMeta {
     return {
       name: 'Cheating bot',
@@ -28,19 +14,11 @@ export class CheatingBot extends ASolverBot {
     };
   }
 
-  protected async _init(): Promise<void> {
-    // init your solver bot here
-    const date = new Date();
-    const formatted = date.toISOString().substring(0, 10);
-    const endpoint = `https://www.nytimes.com/svc/wordle/v2/${formatted}.json`;
-
-    this.correctWord = await fetch(endpoint)
-      .then((response) => response.json())
-      .then((response) => response.solution);
-  }
+  protected async _init(): Promise<void> {}
 
   protected async _pickWord(): Promise<string> {
-    // write your custom bot logic here
-    return this.correctWord ?? 'cheats';
+    // Fun fact, in Typescript, private fields are not actually private :^)
+    // It's just a compile-time check that can be easily bypassed
+    return (this as unknown as FakeItTillYouMakeIt)._wordle._solution.join('');
   }
 }
